b6859d327054686c4f1c340d34573e596008eec7
Rupesh <np03a190029@heraldcollege.edu.np>
2023-01-11 15:44:34 +0545

    initial commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -57,8 +57,6 @@ table,  tr {
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">       
                 <table>
-                    
-                    ddddd
                 
                     <tr>
                         <th>Id</th>
